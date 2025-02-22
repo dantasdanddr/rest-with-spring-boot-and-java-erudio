36a87c798ef3a58efeaaa2628aa110f1277ba920
dantasdanddr_win11 <dantas.dan.ddr@gmail.com>
2025-02-22 15:43:01 -0300

    Working with four HTTP Main Verbs - GET, POST, PUT and Delete

---
+++ 06_WorkingWithVerbs/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/dantas/controllers/PersonController.java
@@ -0,0 +1,58 @@
+package br.com.dantas.controllers;
+
+import br.com.dantas.model.Person;
+import br.com.dantas.service.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person findById(@PathVariable("id") String id){
+        return service.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll(){
+        return service.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person create(@RequestBody Person person){
+        return service.create(person);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person update(@RequestBody Person person){
+        return service.update(person);
+    }
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.DELETE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") String id){
+        service.delete(id);
+    }
+}
---
+++ 06_WorkingWithVerbs/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/dantas/service/PersonServices.java
@@ -0,0 +1,65 @@
+package br.com.dantas.service;
+
+import br.com.dantas.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+    private final Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person findById(String id) {
+        logger.info("Finding one Person!");
+
+        var person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Daniel");
+        person.setLastName("Rodrigues");
+        person.setAddress("Suzano - SP");
+        person.setGender("male");
+        return person;
+    }
+
+    public List<Person> findAll() {
+        logger.info("Finding people!");
+
+        var persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    public Person create(Person person) {
+        logger.info("Creating one Person!");
+
+        return person;
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one Person!");
+
+        return person;
+    }
+
+    public void delete(String id) {
+        logger.info("Deleting person from id " + id + "!");
+    }
+
+    private Person mockPerson(int i) {
+        var person = new Person();
+        person.setId((long) i);
+        person.setFirstName("First Name - " + i);
+        person.setLastName("Last Name - " + i);
+        person.setAddress("Address - " + i);
+        person.setGender((i % 2 == 0) ? "male" : "Female");
+        return person;
+    }
+}
