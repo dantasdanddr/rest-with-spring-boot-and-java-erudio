4816da723b1c5eed940be1d9555ddc21108b22c4
dantasdanddr_win11 <dantas.dan.ddr@gmail.com>
2025-02-26 17:33:12 -0300

    Custom JSON serialization

---
+++ 12_CustomSerialization/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/dantas/controllers/PersonController.java
@@ -0,0 +1,58 @@
+package br.com.dantas.controllers;
+
+import br.com.dantas.data.dto.PersonDTO;
+import br.com.dantas.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    @GetMapping(value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public PersonDTO findById(@PathVariable("id") Long id){
+        var person = service.findById(id);
+        person.setBirthDay(new Date());
+        //person.setPhoneNumber("+55 (11) 98765-4321");
+        person.setPhoneNumber("");
+        person.setLastName(null);
+        person.setSensitiveData("Foo Bar");
+        return person;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<PersonDTO> findAll(){
+        return service.findAll();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public PersonDTO create(@RequestBody PersonDTO person){
+        return service.create(person);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public PersonDTO update(@RequestBody PersonDTO person){
+        return service.update(person);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
---
+++ 12_CustomSerialization/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/dantas/Startup12Tests.java
@@ -0,0 +1,13 @@
+package br.com.dantas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Startup12Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
