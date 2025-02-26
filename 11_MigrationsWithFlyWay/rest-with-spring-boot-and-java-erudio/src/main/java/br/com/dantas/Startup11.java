d23492ce9225e6fc2a3c8e4de63bbebc81c9fc08
dantasdanddr_win11 <dantas.dan.ddr@gmail.com>
2025-02-26 15:52:15 -0300

    Adding migrations support with Flyway

---
+++ 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/dantas/Startup11.java
@@ -0,0 +1,13 @@
+package br.com.dantas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup11 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup11.class, args);
+	}
+
+}
---
+++ 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/dantas/Startup11Tests.java
@@ -0,0 +1,13 @@
+package br.com.dantas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Startup11Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
